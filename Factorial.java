764cb1c4b4942ac7df1d590cb11e79ebc4a92756
Tanzilatisha123 <tanzila29092006@gmail.com>
2026-08-14 14:34:13 +0530

    Fix output message for factorial calculation

--- Factorial.java
+++ Factorial.java
@@ -13,6 +13,6 @@ public class Factorial {
     public static void main(String[] args) {
         int number = 7; // Example number to calculate factorial
         long result = factorial(number); // Call the method
-        System.out.println("Factorial of " + number + " is: " + result);
+        System.out.println("Factorial of number " + number + " is: " + result);
     }
 }
